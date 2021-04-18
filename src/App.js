4edd12f89a186be4f51da094b67348bea74fa492
Ibrahim Mohamed <ibrahimm.mourad97@gmail.com>
2021-04-18 02:52:46 +0200

    task 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
 import "./App.css";
-
-function App() {
-  return <div>Hello</div>;
-}
+import myimg from "./2.jpg";
+const App = () => {
+  return (
+    <>
+      <div style={{ border: "1px solid black", maxWidth: "100vw" }}>
+        <h1 className="title red">Ibrahim</h1>
+        <br />
+        <img src={myimg} alt="myimg" />
+        <br />
+        <img src={"/1.jpg"} alt="myimg" />
+      </div>
+      <video
+        src="myVideo.mp4"
+        type="video/mp4"
+        width="600"
+        height="300"
+        controls="controls"
+        autoplay="true"
+      />
+    </>
+  );
+};
 
 export default App;
